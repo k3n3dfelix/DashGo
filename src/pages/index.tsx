608e12634ea35620e7b156f1dcde3a99c93aeffe
Kened Silva Felix <ksfelix1@stefanini.com>
2023-03-22 13:23:36 -0300

    Feito Component Header da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,17 @@
 import { Inter } from 'next/font/google'
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack, FormLabel } from '@chakra-ui/react';
+import { Input } from '@/components/form/Input';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
         <Stack spacing={4}>
-          <FormControl>
-          <FormLabel htmlFor="email">E-mail</FormLabel>
-          <Input 
-            name="email" 
-            id="email"
-            type="email" 
-            focusBorderColor="pink.500" 
-            bgColor="gray.900" 
-            variant="filled"
-            _hover={{
-              bgColor: 'gray.900'
-            }}
-            size="lg"
-            
-          >
-          </Input>
-          </FormControl>
-          <FormControl>
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input 
-            name="password"
-            type="password"
-            focusBorderColor="pink.500" 
-            bgColor="gray.900" 
-            variant="filled"
-            _hover={{
-              bgColor: 'gray.900'
-            }}
-            size="lg"
-          >
-          </Input>
-          </FormControl>
+          <Input type="email" label="E-mail" name="email"/>
+          <Input type="passsword" label="Senha" name="password" />
          </Stack>
         <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
       </Flex>
