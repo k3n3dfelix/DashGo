9f631b96350c620aaf0e6e5c2e35a2717e942741
Kened Silva Felix <ksfelix1@stefanini.com>
2023-03-22 21:01:59 -0300

    adicionado graficos dashboard + inicio pagina usuários

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ export function Sidebar() {
     <Box as="aside" w="65" mr="8">
       <Stack spacing="12" align="flex-start">
         <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">Geral</Text>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">GERAL</Text>
           <Stack spacing="4" mt="8" align="stretch">
             <Link display="flex" alignItems="center">
               <Icon as={RiDashboardLine} fontSize="20" />
