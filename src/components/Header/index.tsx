98468b1365347a53e7170abaca99c00784102125
Kened Silva Felix <ksfelix1@stefanini.com>
2023-04-24 22:55:09 -0300

    adicioando responsividade SideBar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { useSidebarDrawer } from "@/contexts/SideBarDrawerContext";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
 
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
@@ -6,10 +8,13 @@ import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+  
   return (
     <Flex
       as="header"
@@ -21,6 +26,18 @@ export function Header() {
       px="6"
       align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="open navigation"
+          bg="transparent"
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+          icon={<Icon as={RiMenuLine} 
+          />}
+        ></IconButton>
+      )}
       <Logo />
       { isWideVersion && <SearchBox />}
       <Flex align="center" ml="auto">
