3a801afbbc6b7d6ed1359f9192f91960da8d4b2f
Kened Silva Felix <ksfelix1@stefanini.com>
2023-04-24 22:24:26 -0300

    Adicionando responsividade a Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-          <Box mr="4" textAlign="right">
-            <Text>Kened Felix</Text>
-            <Text color="gray.300" fontSize="small">kened.felix@gmail.com.com</Text>
-          </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Kened Felix</Text>
+          <Text color="gray.300" fontSize="small">
+            kened.felix@gmail.com.com
+          </Text>
+        </Box>
+      )}
 
-          <Avatar size="md" name="Diego Fernandes" src="https://github.com/k3n3dfelix.png"/>
-        </Flex>
+      <Avatar
+        size="md"
+        name="Diego Fernandes"
+        src="https://github.com/k3n3dfelix.png"
+      />
+    </Flex>
   );
 }
